00c3dd79d132847df5e621465291f8b94d9aece8
Ethan Dang <tiencuong.dang@gmail.com>
2019-08-13 17:07:35 +0700

    between and next_index

--- lib/reorderex.ex
+++ lib/reorderex.ex
@@ -3,16 +3,90 @@ defmodule Reorderex do
   Documentation for Reorderex.
   """
 
+  @digits '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+  @zero List.first(@digits)
+  # @max_digit List.last(@digits)
+  @digits_len length(@digits)
+
   @doc """
-  Hello world.
+  Between.
 
   ## Examples
 
-      iex> Reorderex.hello()
-      :world
+      iex> Reorderex.between("a", "c")
+      "b"
+
+      iex> Reorderex.between("a", "a")
+      "a"
 
   """
-  def hello do
-    :world
+  def between(a, b) when is_binary(a) and is_binary(b) do
+    try do
+      between!(a |> to_charlist, b |> to_charlist)
+      |> to_string
+    rescue
+      _e in ArgumentError -> a
+    end
+  end
+
+  def between!([fa | ra] = a, [fb | rb]) do
+    if fa == fb do
+      [fa | between!(ra, rb)]
+    else
+      pa = @digits |> Enum.find_index(&(&1 == fa))
+      pb = @digits |> Enum.find_index(&(&1 == fb))
+
+      cond do
+        pa + 1 < pb ->
+          [@digits |> Enum.at(div(pa + pb, 2))]
+
+        pa + 1 == pb ->
+          a ++ [Enum.at(@digits, div(@digits_len, 2))]
+          # [fa | between!(ra, [@max_digit])]
+      end
+    end
+  end
+
+  def between!([], [_ | _] = b), do: between!([@zero], b)
+  def between!([_ | _] = a, []), do: between!(a, [@zero])
+
+  def between!([], []) do
+    raise ArgumentError, message: "Given numbers are equals"
+  end
+
+  def filter(s) do
+    s
+    |> to_charlist()
+    |> Enum.filter(&(&1 in @digits))
+    |> Enum.reverse()
+    |> Enum.reduce([], fn el, acc ->
+      if not Enum.empty?(acc) or el != Enum.at(@digits, 0) do
+        [el | acc]
+      else
+        acc
+      end
+    end)
+    |> to_string()
+  end
+
+  @doc """
+  Convert.
+
+  ## Examples
+
+      iex> Reorderex.convert(62)
+      '10'
+
+  """
+
+  def convert(num), do: convert_acc(num, [])
+
+  defp convert_acc(0, acc), do: acc
+
+  defp convert_acc(r, acc),
+    do: convert_acc(div(r, @digits_len), [@digits |> Enum.at(rem(r, @digits_len)) | acc])
+
+  def next_index(_count \\ 1) do
+    :os.system_time(:millisecond) |> convert
   end
 end
--- test/reorderex_test.exs
+++ test/reorderex_test.exs
@@ -1,8 +1,68 @@
 defmodule ReorderexTest do
   use ExUnit.Case
+  use ExUnitProperties
+
   doctest Reorderex
 
-  test "greets the world" do
-    assert Reorderex.hello() == :world
+  describe "filter/1" do
+    test "abc" do
+      assert Reorderex.filter("abc00") == "abc"
+      assert Reorderex.filter("abc000") == Reorderex.filter("abc00")
+    end
+  end
+
+  describe "between/2" do
+    test "should produce result that stays in between" do
+      check all(
+              a <- StreamData.string(:ascii, max_length: 10),
+              b <- StreamData.string(:ascii, max_length: 10),
+              max_run: 1000
+            ) do
+        a = Reorderex.filter(a)
+        b = Reorderex.filter(b)
+
+        if a != b do
+          [a, b] = if a > b, do: [b, a], else: [a, b]
+
+          # [a, b] |> IO.inspect()
+
+          avg = Reorderex.between(a, b)
+          # |> IO.inspect()
+
+          assert a < avg
+          assert avg < b
+          assert String.length(avg) <= max(String.length(a), String.length(b)) + 1
+        else
+          avg = Reorderex.between(a, b)
+
+          assert a == avg
+          assert avg == b
+        end
+      end
+    end
+
+    test "should survive edge cases" do
+      for [a, b] <- [
+            ["Jh", "K"],
+            ["Jz", "K"],
+            ["a", "a1"],
+            ["a", "a01"]
+          ] do
+        avg = Reorderex.between(a, b)
+
+        assert a < avg
+        assert avg < b
+        assert String.length(avg) <= max(String.length(a), String.length(b)) + 1
+      end
+    end
+  end
+
+  describe "next_index/1" do
+    test "should be increasing" do
+      a = Reorderex.next_index()
+      :timer.sleep(10)
+      b = Reorderex.next_index()
+      assert a < b
+    end
   end
 end
