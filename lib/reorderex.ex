7712ddc4ede073999c34089c30acd7210a8ed8e3
Ethan Dang <tiencuong.dang@gmail.com>
2019-08-13 14:00:14 +0700

    init

---
+++ lib/reorderex.ex
@@ -0,0 +1,18 @@
+defmodule Reorderex do
+  @moduledoc """
+  Documentation for Reorderex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Reorderex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/reorderex_test.exs
@@ -0,0 +1,8 @@
+defmodule ReorderexTest do
+  use ExUnit.Case
+  doctest Reorderex
+
+  test "greets the world" do
+    assert Reorderex.hello() == :world
+  end
+end
